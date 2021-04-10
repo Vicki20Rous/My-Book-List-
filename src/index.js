f698d622b8b974877fd6746311e8fbeb93397e5c
GoddessVictoria <vic86rous@gmail.com>
2021-04-10 13:02:11 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import LogRocket from 'logrocket';
+
+// LogRocket.init('1hwfla/stack_tutorial');
 
 ReactDOM.render(
   <React.StrictMode>
